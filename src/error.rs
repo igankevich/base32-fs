6afcf64da0c9c6129e320d7ffc0fcb0b78aa5776
Ivan Gankevich <ivan@igankevich.com>
2025-04-15 21:43:46 +0200

    Enable `doc_cfg`.

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@
 pub struct DecodeError;
 
 #[cfg(feature = "std")]
-#[cfg_attr(any(docsrs), doc(cfg(feature = "std")))]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
 impl std::error::Error for DecodeError {}
 
 impl core::fmt::Display for DecodeError {
